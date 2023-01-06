56822ece192cc6e48cc5c7c67ef0643649e31239
vinayaka-iyer <vinayakaiyer999@gmail.com>
2023-01-06 21:08:16 +0300

    Questions in Store value

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import data from "../database/data";
+import { useSelector } from "react-redux";
 
 /** custom hook */
 import { useFetchQuestion } from "../hooks/FetchQuestion";
@@ -7,22 +7,28 @@ import { useFetchQuestion } from "../hooks/FetchQuestion";
 const Questions = () => {
   const [checked, setChecked] = useState(undefined);
   const [{ isLoading, apiData, serverError }] = useFetchQuestion();
-  const question = data[0];
+
+  const questions = useSelector(
+    (state) => state.questions.queue[state.questions.trace]
+  );
 
   useEffect(() => {
-    console.log(isLoading);
-    // console.log(apiData);
-    // console.log(serverError);
+    console.log(questions);
   });
 
   const onSelect = () => {
     // console.log("Radio button change");
   };
+
+  if (isLoading) return <h3 className="text-light">isLoading</h3>;
+  if (serverError)
+    return <h3 className="text-light">{serverError || "Unknown Error "}</h3>;
+
   return (
     <div className="questions">
-      <h2 className="text-light">{question.question}</h2>
-      <ul key={question.id}>
-        {question.options.map((q, i) => (
+      <h2 className="text-light">{questions?.question}</h2>
+      <ul key={questions?.id}>
+        {questions?.options.map((q, i) => (
           <li key={i}>
             <input
               type="radio"
